19de8912b6425a2226560c76a34ec1b4abc15ccf
dave glynn <dave@ocl.ie>
2016-10-05 16:01:27 +0100

    Before importing Auth code

--- app/security/security.module.ts
+++ app/security/security.module.ts
@@ -4,7 +4,6 @@ import { CommonModule }        from '@angular/common';
 import { FormsModule, ReactiveFormsModule, } from '@angular/forms';
 import { PreventUnsavedChangesGuard } from '../prevent-unsaved-changes-guard.service';
 import { SharedModule }        from '../shared/shared.module';
-
 import { RouterModule }        from '@angular/router';
 import { HttpModule }          from '@angular/http'; 
 
@@ -12,11 +11,10 @@ import { User }                from './users/user';
 import { UserFormComponent }   from './users/user-form.component';
 import { UsersComponent }      from './users/users.component';
 import { TableSimpleComponent }    from '../shared/components/tableSimple.component';
-/* import { UserService }         from './user.service'; */
+
 import { ErrorService } from ".././errors/error.service";
 import { ProfileService } from '../master/profiles/profile.service';
 import { LanguageService } from '../master/languages/language.service';
-import { CommonService } from   '../shared/helpers/common.service'; 
 
 @NgModule({
     imports: [
@@ -27,7 +25,7 @@ import { CommonService } from   '../shared/helpers/common.service';
         HttpModule,
         SharedModule 
     ],
-    declarations: [
+    declarations: [ 
         UserFormComponent, 
         UsersComponent,
         TableSimpleComponent
